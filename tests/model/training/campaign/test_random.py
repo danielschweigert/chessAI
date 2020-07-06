92c81fe6b8889e2b4c4875632091354fcbe0fafc
danielschweigert <dan.schweigert@gmail.com>
2020-07-06 12:38:05 -0700

    complete test

--- tests/model/training/campaign/test_random.py
+++ tests/model/training/campaign/test_random.py
@@ -1,7 +1,9 @@
 import unittest
 import chess
 from chessAI.model.training.campaign.random import RandomWalkTrainingCampaign
+from chessAI.model.evaluation.fcn.simpleFCNEvaluation import SimpleFCNEvaluator
 from chessAI.model.evaluation.cnn.simpleCNNEvaluation import SimpleCNNEvaluator
+
 from chessAI.model.play.shallow import ShallowPlayer
 from chessAI.util.logging import DataFileLogger
 from config import config
@@ -9,7 +11,7 @@ from config import config
 
 class RandomWalkTrainingCampaignTest(unittest.TestCase):
 
-    def test_random_walk_training_campaign(self):
+    def test_random_walk_training_campaign_cnn(self):
 
         evaluator = SimpleCNNEvaluator()
         player = ShallowPlayer(evaluator)
@@ -26,11 +28,10 @@ class RandomWalkTrainingCampaignTest(unittest.TestCase):
         initial_board.set_piece_at(square=chess.H4, piece=chess.Piece.from_symbol('q'))
         initial_board.set_piece_at(square=chess.G4, piece=chess.Piece.from_symbol('q'))
         initial_board.set_piece_at(square=chess.H5, piece=chess.Piece.from_symbol('q'))
-        print(initial_board)
         initial_boards = [initial_board]
         kwargs = {
-            'n_rounds_series': 10,
-            'n_iterations_training': 50,
+            'n_rounds_series': 5,
+            'n_iterations_training': 5,
             'side': 1,
             'random_walk_n_subset': 40,
             'max_abs_rel_change': 0.1,
@@ -38,4 +39,40 @@ class RandomWalkTrainingCampaignTest(unittest.TestCase):
         }
         rwtc = RandomWalkTrainingCampaign(player, engine_player_path, engine_player_time_limit, initial_boards,
                                           **kwargs)
-        rwtc.run()
+        score, parameters = rwtc.run()
+        rwtc.engine_player.close()
+        self.assertIsInstance(score, float)
+        self.assertEqual(n_parameters, len(parameters))
+
+    def test_random_walk_training_campaign_fcn(self):
+
+        evaluator = SimpleFCNEvaluator()
+        player = ShallowPlayer(evaluator)
+        n_parameters = len(evaluator.get_parameters())
+        engine_player_path = config['test']['uci_engine_path']
+        engine_player_time_limit = 0.1
+        data_file_logger = DataFileLogger(config['test']['data_file_logger_path'], n_parameters=n_parameters,
+                                          append=True)
+
+        initial_board = chess.Board()
+        initial_board.clear_board()
+        initial_board.set_piece_at(square=chess.C4, piece=chess.Piece.from_symbol('K'))
+        initial_board.set_piece_at(square=chess.F4, piece=chess.Piece.from_symbol('k'))
+        initial_board.set_piece_at(square=chess.H4, piece=chess.Piece.from_symbol('q'))
+        initial_board.set_piece_at(square=chess.G4, piece=chess.Piece.from_symbol('q'))
+        initial_board.set_piece_at(square=chess.H5, piece=chess.Piece.from_symbol('q'))
+        initial_boards = [initial_board]
+        kwargs = {
+            'n_rounds_series': 5,
+            'n_iterations_training': 5,
+            'side': 1,
+            'random_walk_n_subset': 5000,
+            'max_abs_rel_change': 0.1,
+            'data_loggers': [data_file_logger]
+        }
+        rwtc = RandomWalkTrainingCampaign(player, engine_player_path, engine_player_time_limit, initial_boards,
+                                          **kwargs)
+        score, parameters = rwtc.run()
+        rwtc.engine_player.close()
+        self.assertIsInstance(score, float)
+        self.assertEqual(n_parameters, len(parameters))
